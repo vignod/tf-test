565f75e3d6faa3864bc3fd4a73a0cd2da94ae3b7
jackjohn2526 <jackjohn2526@gmail.com>
2025-10-18 16:19:40 +1100

    error fixed

--- vpc.tf
+++ vpc.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "crm_vpc" {
 resource "aws_subnet" "crm-web-sn" {
   vpc_id     = aws_vpc.crm_vpc.id
   cidr_block = "10.0.1.0/24"
-  map_publi_ip_on_launch = "true"
+  map_public_ip_on_launch = "true"
 
   tags = {
     Name = "crm-web-subnet"
@@ -23,7 +23,7 @@ resource "aws_subnet" "crm-web-sn" {
 resource "aws_subnet" "crm-api-sn" {
   vpc_id     = aws_vpc.crm_vpc.id
   cidr_block = "10.0.2.0/24"
-  map_publi_ip_on_launch = "true"
+  map_public_ip_on_launch = "true"
 
   tags = {
     Name = "crm-api-subnet"
